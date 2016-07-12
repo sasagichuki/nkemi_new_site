82699d6f39a27d3a4dad65fd2dd2582d416490e8
Charles Gichuki <sasagichuki@gmail.com>
2016-07-12 17:25:26 +0300

    initial Commit

---
+++ cache/compiled/files/3fb134e4a1fb8e8b9c5953df10d2c600.yaml.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/chaos/projects/nkemi_new_site/user/themes/antimatter/languages.yaml',
+    'modified' => 1461737248,
+    'data' => [
+        'en' => [
+            'TRANSLATION_TEST' => 'Antimatter!'
+        ]
+    ]
+];

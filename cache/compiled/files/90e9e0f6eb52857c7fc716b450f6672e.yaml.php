6dbd3f593349b168395aeb770316d4ed6d762ff0
Charles Gichuki <sasagichuki@gmail.com>
2016-07-14 16:51:45 +0300

    Testing deletion

--- cache/compiled/files/90e9e0f6eb52857c7fc716b450f6672e.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/chaos/projects/nkemi_new_site/user/config/media.yaml',
-    'modified' => 1468251939,
-    'data' => [
-        
-    ]
-];
